97c82c9dc9409432cf92d7b843469ba22dcdd665
Billy Hao <billyhao12@gmail.com>
2020-11-11 00:19:12 -0800

    Initial files with some completed tests.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testing Updated</h1>
+      <p>Addition</p>
+    </div>
+  );
+}
+
+
+export default App;
