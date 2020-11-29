26ebb6101987480f78bde1e172394ab9474d01fd
Billy Hao <billyhao12@gmail.com>
2020-11-28 23:28:17 -0800

    Test if counter in context increments and decrements correctly.

--- src/components/TextContext.test.js
+++ src/components/TextContext.test.js
@@ -19,3 +19,17 @@ it('checks if initial state is equal to 0', () => {
     const { getByTestId } = renderWithContext(<Counter />);
     expect(getByTestId('counter')).toHaveTextContent('0');
 });
+
+it('increments the counter', () => {
+    const { getByTestId } = renderWithContext(<Counter />);
+
+    fireEvent.click(getByTestId('button-up'));
+    expect(getByTestId('counter')).toHaveTextContent('1');
+});
+
+it('decrements the counter', () => {
+    const { getByTestId } = renderWithContext(<Counter />);
+
+    fireEvent.click(getByTestId('button-down'));
+    expect(getByTestId('counter')).toHaveTextContent('-1');
+});
